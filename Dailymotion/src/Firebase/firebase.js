0cc5dca6a2dd29a122338be9d12addd906a288a1
Partha Debnath <85936227+IM-Partha@users.noreply.github.com>
2025-08-24 00:32:00 +0530

    Update firebase.js

--- Dailymotion/src/Firebase/firebase.js
+++ Dailymotion/src/Firebase/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDVBsP-oadfrK8J2W7ikLxivDb1u7psqqA",
   authDomain: "dailymotion-28f13.firebaseapp.com",
   projectId: "dailymotion-28f13",
-  storageBucket: "dailymotion-28f13.firebasestorage.app",
+  storageBucket: "dailymotion-28f13.appspot.com",
   messagingSenderId: "624287339591",
   appId: "1:624287339591:web:1910c10f160eb9341c0d62",
   measurementId: "G-GZ2LZQ8G3J"
